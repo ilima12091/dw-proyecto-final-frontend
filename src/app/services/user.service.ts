36cb69d0381d15efbb066387f79d8b240e14afd7
Valeria <valecardozog@gmail.com>
2023-07-01 09:44:50 -0300

    basic user registration working

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  commonHeaders = {
+    'Content-Type': 'application/json',
+    Accept: '*/*',
+  };
+  url = 'http://localhost:8000';
+
+  constructor() { }
+
+  async getUsers(): Promise<User[]> {
+    return (await (await fetch(`${this.url}/users`)).json()) ?? [];
+  }
+  
+  async createUser(user: User): Promise<boolean> {
+    const result= await fetch(`${this.url}/register`,{
+      method: 'POST',
+      headers:this.commonHeaders,
+      body:JSON.stringify(user),
+    });
+    return result.ok;
+  }
+
+  async modifyUser(user: User): Promise<boolean> {
+    const result= await fetch('${this.url}/user/${user.username}',{
+      method: 'PUT',
+      headers:this.commonHeaders,
+      body:JSON.stringify(user),
+    });
+    return result.ok;
+  }
+}
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  
+  name?: string;
+  surname?: string;
+  email?: string;
+  password?: string;
+  username?: string;
 
+  constructor(private userService: UserService) { }
+
+  signUp(): void {
+
+    const user : User = {
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      password: this.password,
+      username: this.username
+    };
+
+    this.userService.createUser(user).then(response => {
+        
+        console.log('User created successfully', response);
+      })
+      .catch(error => {
+        console.error('Error creating user', error);
+        
+      });
+  }
 }
