bc27c4a7c16e64d26a5e5bf731392361eede11ef
RobottiFranco <franco.robotti0@gmail.com>
2023-07-02 19:10:54 -0300

    fix service post

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -19,4 +19,42 @@ export class PostsService {
   async getSpecificPost(postId: number): Promise<PostCard> {
     return this.secureService.request('GET', `${BASE_URL}/posts/${postId}`);
   }
+  
+  async createPost(post: PostCard): Promise<PostCard> {
+    const response = await this.secureService.request('POST', `${BASE_URL}/users/${post.userId}/posts`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    });
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error creating post');
+    }
+  }
+
+  async modifyPost(post: PostCard): Promise<PostCard> {
+    const response = await this.secureService.request('PUT', `${BASE_URL}/users/${post.userId}/posts/${post.postId}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    });
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error updating post');
+    }
+  }
+
+  async deletePost(post: PostCard): Promise<void> {
+    const response = await this.secureService.request('DELETE', `${BASE_URL}/users/${post.userId}/posts/${post.postId}`);
+
+    if (!response.ok) {
+      throw new Error('Error deleting post');
+    }
+  }
 }
